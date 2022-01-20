26a4b52a5414ec8af21cb96b5003fe7381ceb6ce
HussainS12 <88190528+HussainS12@users.noreply.github.com>
2022-01-20 15:07:06 +0530

    Update infrostructure.tf

--- infrostructure.tf
+++ infrostructure.tf
@@ -12,7 +12,7 @@ resource "aws_lb_listener" "front_end" {
   load_balancer_arn = aws_lb.front_end.arn
   port              = "80"
   protocol          = "HTTP"
-   
+    
   default_action {
     type = "redirect"
 
